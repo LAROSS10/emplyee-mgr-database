0d2e14590721bca71971c844638397894c180faf
Lori Ross <laross70@gmail.com>
2020-03-29 08:06:51 -0500

    functions to view departmments, roles and titles are now created and functional

--- index.js
+++ index.js
@@ -15,21 +15,40 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id" + connection.threadId);
     // start the application by running start function below
-    selectEmployee();
+    viewDepartment();
+    viewEmployees();
+    viewRoles();
     
 });
 
-    function selectEmployee() {
+    function viewDepartment() {
         connection.query("SELECT * FROM department", function(err,res){
             if (err) throw err;
             for (let i=0; i<res.length; i++) {
                 console.log(res[i].department_name)
             }
-
-
-            // console.log(res);
-            connection.end;
+            // console.log(res);            
         })
     };
     
-         
+    function viewEmployees() {
+        connection.query("SELECT * FROM employee", function(err,res){
+            if (err) throw err;
+            for (let i=0; i<res.length; i++) {
+                console.log(res[i].first_name + " " + " " +  res[i].last_name)
+            }
+            // console.log(res);
+            connection.end;
+        })
+    }; 
+
+    function viewRoles() {
+        connection.query("SELECT * FROM title", function(err,res){
+            if (err) throw err;
+            for (let i=0; i<res.length; i++) {
+                console.log(res[i].title + " " + " " +  res[i].salary)
+            }
+            // console.log(res);
+            connection.end;
+        })
+    }; 
